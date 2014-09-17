f19deda77c6581d55c3d9bdeaa5418dcb2371b25
Niklas Rehfeld <niklas.rehfeld@gmail.com>
2014-09-17 13:49:18 +1200

    Added package docs and moved URLs into here.

--- __init__.py
+++ __init__.py
@@ -3,19 +3,12 @@
 # Filename: __init__.py
 # Description: NationBuilder API Library
 # Author: Niklas Rehfeld
-# Maintainer: Niklas Rehfeld
 # Copyright 2014 Niklas Rehfeld
-# Created: Fri Sep  5 14:58:28 2014 (+1200)
-# Version: 0.2
-# Package-Requires: (oauth2client,httplib2,)
 # URL: https://github.com/nikr/nbpy
-# Doc URL: https://github.com/nikr/nbpy/wiki
-# Keywords: NationBuilder
-# Compatibility:
 #
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
+#    Licensed under the Apache License, Version 2.0 (the "License");
 #    you may not use this file except in compliance with the License.
+#
 #    You may obtain a copy of the License at
 #
 #        http://www.apache.org/licenses/LICENSE-2.0
@@ -25,5 +18,51 @@
 #    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
-#
-#
+
+"""
+Package nbpy:
+
+Contains the following modules:
+
+nb_api: contains the NationBuilderApi class, which is used as a base class for
+    the various APIs -- People, Tags, Lists, etc. It also contains the
+    Exception classes that are raised by the API access methods.
+
+nationbuilder: Contains the NationBuilder class which serves as the main entry
+    point for this library. It is recommended to instantiate one of these and
+    use the NationBuilder.people, NationBuilder.tags, etc. members to access
+    the various APIs.
+
+people: Contains the People class, used to access the People API.
+
+tags: Contains the Tags class used to access the Tags API
+
+lists: Contains the NBLists class used to access the Lists API.
+
+This file also includes the definitions for various URLs used to access the
+various NationBuilder APIs.
+
+"""
+
+BASE_URL = 'https://{slug}.nationbuilder.com/api/v1'
+PAGINATE_QUERY = "?page={page}&per_page={per_page}"
+GET_PERSON_URL = BASE_URL + '/people/{0}'
+MATCH_EMAIL_URL = BASE_URL + "/people/match?email={0}"
+UPDATE_PERSON_URL = GET_PERSON_URL
+REMOVE_TAG_URL = UPDATE_PERSON_URL + "/taggings/{1}"
+LIST_TAGS_URL = BASE_URL + "/tags" + PAGINATE_QUERY
+GET_BY_TAG_URL = BASE_URL + "/tags/{tag}/people" + PAGINATE_QUERY
+REGISTER_PERSON_URL = GET_PERSON_URL + "/register"
+
+LIST_INDEX_URL = BASE_URL + '/lists' + PAGINATE_QUERY
+GET_LIST_URL = BASE_URL + '/lists/{list_id}/people' + PAGINATE_QUERY
+
+GET_CONTACT_URL = GET_PERSON_URL + "/contacts"
+
+USER_AGENT = "nbpy/0.2"
+
+HEADERS = {
+    'Content-type': 'application/json',
+    "Accept": "application/json",
+    "User-Agent": USER_AGENT,
+}
